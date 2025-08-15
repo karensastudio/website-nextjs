cad56cb55a64667afc6d32b674ca147c24246290
Faraz <ifarazir@gmail.com>
2025-08-16 00:26:14 +0330

    fix semicolon bug

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -96,9 +96,7 @@ export default function Layout({ children }) {
         id="goftino-widget"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: ```
- !function(){var i="DHQHJr",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
- ```,
+          __html: `!function(){var i="DHQHJr",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();`,
         }}
       />
     </html>
